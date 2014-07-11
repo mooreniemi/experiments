688684d40bb35c05fd5e33449ef4be22c58f5781
Alex Moore-Niemi <alex@promoboxx.com>
2014-07-10 23:38:11 -0400

    finally applying at the memo

--- curried_summer.rb
+++ curried_summer.rb
@@ -27,4 +27,24 @@ RSpec.describe "currying experiment" do
       expect(partial[summer]).to eq(15)
     end
   end
+
+  context "with fully abstracted injection" do
+    odds_proc = Proc.new { |a, e| e.odd? ? a << e : a }
+    rap = Proc.new {|collection, proc, memo| collection.inject memo, &proc}
+    curried = rap.curry
+
+    it "should simply return proc with only one argument" do
+      expect(curried[(1..10)]).to be_a_kind_of(Proc)
+    end
+
+    it "should simply return proc when two arguments are passed in" do
+      expect(curried[(1..10)][odds_proc]).to be_a_kind_of(Proc)
+    end
+
+    it "should easily allow me to do function application" do
+      partial = curried[(1..10)][odds_proc]
+      odds = partial[[]]
+      expect(odds).to eq([1,3,5,7,9])
+    end
+  end
 end
