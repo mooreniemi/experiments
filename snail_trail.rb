f938de199a0ad78518de15d9fcdb36974685c369
Alex Moore-Niemi <amooreniemi@zipcar.com>
2014-09-16 15:36:05 -0400

    converting to back and forth edges

--- snail_trail.rb
+++ snail_trail.rb
@@ -1,45 +1,81 @@
-def snail(collection)
-  slime = []
-  r_perimeter = collection.inject([]) {|a,e| a << e.last}
-  l_perimeter = collection.inject([]) {|a,e| a << e.first}
-  size = collection.size
+require 'pry'
 
-  case size
-  when 1
-    slime << collection
-  when 2
-    slime << collection[0]
-    slime << collection[1].reverse
-  when 3
-    slime << collection[0]
-    slime << r_perimeter[1..-1]
-    slime << collection[2][0..1].reverse
-    slime << collection[1][0..1]
-  when 4
-    slime << collection[0]
-    slime << r_perimeter[1..-1]
-    slime << collection[3].reverse[1..2]
-    slime << l_perimeter[1..3].reverse
-    slime << collection[1][1..2]
-    slime << collection[2][1..2].reverse
-  else
-    puts "not yet"
+class Trailer
+
+  def initialize(collection)
+    @collection = collection
+    @slime = []
   end
 
-  slime.flatten
+  def snail
+    return @slime if @collection.flatten.empty?
+    @slime << right_corner
+
+    @collection.delete_at(0) # delete top row
+    @collection = @collection.each {|a| a.delete_at((@collection.length - 1)) } # delete rightmost column
+
+    @slime << left_corner
+    @slime.flatten
+  end
+
+  private
+  def right_corner
+    first_row[0...-1] + last_column
+  end
+
+  def left_corner
+    last_row[0...-1] + first_column
+  end
+
+  def last_column
+    @collection.inject([]) {|a,e| a << e.last }
+  end
+
+  def first_row
+    @collection.first
+  end
+
+  def first_column
+    @collection.inject([]) {|a,e| a << e.first }
+  end
+
+  def last_row
+    @collection.last
+  end
 end
 
 RSpec.describe "snail trail algorithm" do
   it "returns empty array" do
-    expect(snail([[]])).to eq([])
+    expect(Trailer.new([[]]).snail).to eq([])
   end
-  it "can flatten the output" do
-    expect(snail([[1,2],[3,4]])).to eq([1,2,4,3])
+
+  it "takes right corners" do
+    expect(Trailer.new([[1,2],[3,4]]).send(:right_corner)).to eq([1,2,4])
   end
-  it "reverses at end" do
-    expect(snail([[1,2,3],[4,5,6],[7,8,9]])).to eq([1,2,3,6,9,8,7,4,5])
+    it "takes left corners" do
+    expect(Trailer.new([[1,2],[3,4]]).send(:left_corner)).to eq([1,3,4])
   end
-  it "handles 4" do
-    expect(snail([[1,2,3,1],[4,5,6,4],[7,8,9,7],[7,8,9,7]])).to eq([1,2,3,1,4,7,7,9,8,7,7,4,5,6,9,8])
+
+  it "handles 2x2" do
+    expect(Trailer.new([[1,2],
+                        [3,4]]).snail).to eq([1,2,4,3])
+  end
+  it "handles 3x3" do
+    expect(Trailer.new([[1,2,3],
+                        [4,5,6],
+                        [7,8,9]]).snail).to eq([1,2,3,6,9,8,7,4,5])
+  end
+  it "handles 4x4" do
+    expect(Trailer.new([[1,2,3,1],
+                        [4,5,6,4],
+                        [7,8,9,7],
+                        [7,8,9,7]]).snail).to eq([1,2,3,1,4,7,7,9,8,7,7,4,5,6,9,8])
+  end
+  it "handles 5x5" do
+    expect(Trailer.new([[1,2,3,4,5],
+                        [6,7,8,9,10],
+                        [11,12,13,14,15],
+                        [16,17,18,19,20],
+                        [21,22,23,24,25]]).snail).to eq([1,2,3,4,5,10,15,20,25,24,23,22,21,16,11,6,7,8,9,14,19,18,17,12,13])
   end
 end
