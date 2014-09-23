f4c0885dae00028cb498ae6cb1426847e4b4cb6a
Alex Moore-Niemi <alex@promoboxx.com>
2014-09-22 22:55:59 -0400

    rosetta refactoring

--- range_extraction.rb
+++ range_extraction.rb
@@ -3,22 +3,52 @@ require 'pry'
 
 def solution(list)
   return "" if list.empty?
-  
+
   list_of_lists = list.inject([]) do |m,e|
     m << [] if m.empty?
     m.last.last == (e-1) ? m.last << e : m << [e]
     m
   end.reject!(&:empty?)
 
-  list_of_lists.inject([]) do |m,l| 
-    if l.size > 2 
-      m << (l.first.to_s + "-" + l.last.to_s) 
+  list_of_lists.inject([]) do |m,l|
+    if l.size > 2
+      m << (l.first.to_s + "-" + l.last.to_s)
     elsif l.size == 2
-      m << (l.first.to_s + "," + l.last.to_s) 
+      m << (l.first.to_s + "," + l.last.to_s)
     else
       m << l.first.to_s
     end
   end.join(",")
+end
+
+# from rosetta
+# http://rosettacode.org/wiki/Range_extraction#Ruby
+
+def range_extract(l)
+  # pad the list with a big value, so that the last loop iteration will
+  # append something to the range
+  sorted, range = l.sort.concat([Float::MAX]), []
+  canidate_number = sorted.first
+
+  # enumerate over the sorted list in pairs of current number and next by index
+  sorted.each_cons(2) do |current_number, next_number|
+    # if there is a gap between the current element and its next by index
+    if current_number.succ < next_number
+      # if current element is our first or our next by index
+      if canidate_number == current_number
+        # put the first element or next by index into our range as a string
+        range << canidate_number.to_s
+      else
+        # if current element is not the same as the first or next
+        # add [first or next, first or next equals current add , else -, current]
+        seperator = canidate_number.succ == current_number ? "," : "-"
+        range << "%d%s%d" % [canidate_number, seperator, current_number]
+      end
+      # make the first element the next element
+      canidate_number = next_number
+    end
+  end
+  range.join(',')
 end
 
 class Test < Minitest::Test
@@ -47,5 +77,9 @@ class Test < Minitest::Test
 
   def test_canonical
     assert_equal "-6,-3-1,3-5,7-11,14,15,17-20", solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
+  end
+
+  def test_rosetta
+    assert_equal "-6,-3-1,3-5,7-11,14,15,17-20", range_extract([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
   end
 end
