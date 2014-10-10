02f812f473965cdc2f0bb415acdd75d6cca38b71
Alex Moore-Niemi <amooreniemi@zipcar.com>
2014-10-10 15:56:38 -0400

    in progress trying to debug rantly prop test for custom reverse

--- properties.rb
+++ properties.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'rantly'
 require 'rantly/rspec_extensions'
 
@@ -28,17 +29,16 @@ RSpec.describe "general exercise in using a list reversal test via properties" d
   end
 end
 
-RSpec.xdescribe "using custom_reverse and property test" do
+RSpec.describe "using custom_reverse and property test" do
   context "custom reversal" do
-    class Array
-      def custom_reverse
-        #first, *rest = *many
-        new_list = self.empty? ? self : [self.pop]
-        until self.empty?
-          new_list.push(self.pop)
-        end
-        new_list
+    def custom_reverse original_list
+      #first, *rest = *many
+      new_list = original_list.empty? ? original_list : [original_list.pop]
+      until original_list.empty?
+        new_list.push(original_list.pop)
       end
+      #TODO binding.pry
+      new_list
     end
 
     let (:big_size_arrays) do
@@ -47,13 +47,21 @@ RSpec.xdescribe "using custom_reverse and property test" do
     end
 
     it "custom reverses unsorted multi-element array" do
-      expect([1,2,3,5,2].custom_reverse).to eq([2,5,3,2,1])
+      expect(custom_reverse([1,2,3,5,2])).to eq([2,5,3,2,1])
+    end
+
+    it "custom reverses a single element array" do
+      expect(custom_reverse([1])).to eq([1])
+    end
+
+    it "custom reverses returns empty array" do
+      expect(custom_reverse([])).to eq([])
     end
 
     it "the reversed reversal is the same as the original array" do
       property_of(&big_size_arrays).check(50) { |array|
-        reversed_array = array.custom_reverse
-        expect(reversed_array.custom_reverse).to eq(array)
+        reversed_array = custom_reverse(array)
+        expect(custom_reverse(reversed_array)).to eq(array)
       }
     end
   end
