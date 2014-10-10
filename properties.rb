83b190950d2e84a456f56e969f892acc5b96f016
Alex Moore-Niemi <amooreniemi@zipcar.com>
2014-10-10 16:18:40 -0400

    formatting

--- properties.rb
+++ properties.rb
@@ -3,7 +3,7 @@ require 'rantly'
 require 'rantly/rspec_extensions'
 
 RSpec.describe "general exercise in using a list reversal test via properties" do
-  context "regular single case" do
+  context "regular incremental test cases" do
     it "reverses two element array" do
       expect([1,2].reverse).to eq([2,1])
     end
@@ -29,17 +29,19 @@ RSpec.describe "general exercise in using a list reversal test via properties" d
   end
 end
 
-RSpec.describe "using custom_reverse and property test" do
-  context "custom reversal" do
-    def custom_reverse original_list
-      #first, *rest = *many
-      new_list = original_list.empty? ? original_list : [original_list.pop]
-      until original_list.empty?
-        new_list.push(original_list.pop)
-      end
-      new_list
-    end
+# http://www.troikatech.com/blog/2014/04/02/property-based-testing-in-ruby/
+# https://github.com/hayeah/rantly
 
+RSpec.describe "using custom_reverse and property test" do
+  def custom_reverse original_list
+    #first, *rest = *many
+    new_list = original_list.empty? ? original_list : [original_list.pop]
+    until original_list.empty?
+      new_list.push(original_list.pop)
+    end
+    new_list
+  end
+  context "regular incremental test cases" do
     it "custom reverses unsorted multi-element array" do
       expect(custom_reverse([1,2,3,5,2])).to eq([2,5,3,2,1])
     end
@@ -51,14 +53,15 @@ RSpec.describe "using custom_reverse and property test" do
     it "custom reverses returns empty array" do
       expect(custom_reverse([])).to eq([])
     end
-
+  end
+  context "using Rantly" do
     let (:big_size_arrays) do
       valid_size_of_array = -> { Rantly { range 0, 50 } }
       ->(r) { array(valid_size_of_array.call) { integer }}
     end
 
     it "the reversed reversal is the same as the original array" do
-      property_of(&big_size_arrays).check(1) { |array|
+      property_of(&big_size_arrays).check(50) { |array|
         original_array = array.dup
         reversed_array = custom_reverse(array)
         expect(custom_reverse(reversed_array)).to eq(original_array)
