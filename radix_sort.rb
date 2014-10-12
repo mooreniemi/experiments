f059b7b30155024f1bd58d55b1bcd03ffe07fe34
Alex Moore-Niemi <alex@promoboxx.com>
2014-10-12 15:32:38 -0400

    handling negative numbers

--- radix_sort.rb
+++ radix_sort.rb
@@ -10,11 +10,19 @@ module RadixSort
   refine Array do
     def radix_sort
       list_copy = self
+      rounds = Range.new(2,(self.max.to_s.length + 1)).to_a
 
-      number_of_digits = Range.new(2,(self.max.to_s.length + 1))
-
-      number_of_digits.to_a.each do |digit|
+      rounds.each do |digit|
         radices = {
+          -1=>[],
+          -2=>[],
+          -3=>[],
+          -4=>[],
+          -5=>[],
+          -6=>[],
+          -7=>[],
+          -8=>[],
+          -9=>[],
           0=>[],
           1=>[],
           2=>[],
@@ -29,7 +37,9 @@ module RadixSort
         reduction_factor = (digit - 1).digits
 
         list_copy.each do |element|
-          radices[(element % digit.digits)/reduction_factor].push(element)
+          position = (element % digit.digits)/reduction_factor
+          position = -position if element < 0
+          radices[position].push(element)
         end
         list_copy = radices.delete_if { |k, v| v.empty? }.values.flatten
       end
@@ -53,5 +63,10 @@ RSpec.describe "Radix Sort" do
 
   it "sorts third digit of number by bucket" do
     expect([132,908,322,909,44,1].radix_sort).to eq([1,44,132,322,908,909])
+    expect([170, 45, 75, 90, 2, 24, 802, 66].radix_sort).to eq([2, 24, 45, 66, 75, 90, 170, 802])
+  end
+
+  it "works with negative numbers" do
+    expect([170, 45, 75, 90, 2, 24, -802, -66].radix_sort).to eq([-802, -66, 2, 24, 45, 75, 90, 170])
   end
 end
