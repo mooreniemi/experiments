5de82c6e66755f102dddd9ba36d1d6faabef680b
Alex Moore-Niemi <alex@promoboxx.com>
2014-10-12 15:10:55 -0400

    working up to 3 digits confirmed by test

--- radix_sort.rb
+++ radix_sort.rb
@@ -9,29 +9,32 @@ module RadixSort
 
   refine Array do
     def radix_sort
-      radices = {
-        0=>[],
-        1=>[],
-        2=>[],
-        3=>[],
-        4=>[],
-        5=>[],
-        6=>[],
-        7=>[],
-        8=>[],
-        9=>[]
-      }
+      list_copy = self
 
       number_of_digits = Range.new(2,(self.max.to_s.length + 1))
 
       number_of_digits.to_a.each do |digit|
-        reduction_factor = ("%g" % digit).to_i
-        self.each do |element|
+        radices = {
+          0=>[],
+          1=>[],
+          2=>[],
+          3=>[],
+          4=>[],
+          5=>[],
+          6=>[],
+          7=>[],
+          8=>[],
+          9=>[]
+        }
+        reduction_factor = (digit - 1).digits
+
+        list_copy.each do |element|
           radices[(element % digit.digits)/reduction_factor].push(element)
         end
+        list_copy = radices.delete_if { |k, v| v.empty? }.values.flatten
       end
 
-      radices.delete_if { |k, v| v.empty? }.values.flatten
+      list_copy
     end
   end
 
@@ -44,7 +47,11 @@ RSpec.describe "Radix Sort" do
     expect([1,9,3,4].radix_sort).to eq([1,3,4,9])
   end
 
-  xit "sorts second digit of number by bucket" do
+  it "sorts second digit of number by bucket" do
     expect([12,90,32,44].radix_sort).to eq([12,32,44,90])
+  end
+
+  it "sorts third digit of number by bucket" do
+    expect([132,908,322,909,44,1].radix_sort).to eq([1,44,132,322,908,909])
   end
 end
