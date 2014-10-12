9ceb2e6c9e6cd956c6a5b20428a06f58101ff5bb
Alex Moore-Niemi <alex@promoboxx.com>
2014-10-12 14:03:36 -0400

    forgot 0th bucket

--- radix_sort.rb
+++ radix_sort.rb
@@ -2,6 +2,7 @@ module RadixSort
   refine Array do
     def radix_sort
       buckets = {
+        0=>[],
         1=>[],
         2=>[],
         3=>[],
@@ -14,10 +15,10 @@ module RadixSort
       }
 
       self.each do |element|
-        buckets[(element % 10)] = element
+        buckets[(element % 10)].push(element)
       end
 
-      buckets.delete_if { |k, v| v.is_a? Array }.values
+      buckets.delete_if { |k, v| v.empty? }.values.flatten
     end
   end
 end
@@ -28,7 +29,7 @@ RSpec.describe "Radix Sort" do
   it "sorts first digit of number by bucket" do
     expect([1,9,3,4].radix_sort).to eq([1,3,4,9])
   end
-  xit "sorts second digit of number by bucket" do
-    expect([12,90,32,44].radix_sort).to eq([12,90,32,44])
+  it "sorts second digit of number by bucket" do
+    expect([12,90,32,44].radix_sort).to eq([12,32,44,90])
   end
 end
