817aa3d352ef7b5b4e6e952bf94c4bca3c24cc38
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-04-04 20:17:12 -0400

    not doublechecked

--- spec/tree.rb
+++ spec/tree.rb
@@ -26,7 +26,10 @@ class Node
     puts inorder(current.right) unless current.right.nil?
   end
 
-  def postorder
+  def postorder(current = self)
+    puts postorder(current.left) unless current.left.nil?
+    puts postorder(current.right) unless current.right.nil?
+    puts current.value
   end
 end
 
@@ -36,7 +39,20 @@ include TreeData
 describe "Tree" do
   describe "#preorder" do
     it 'outputs values' do
+      # [ 6, 3, 5, 9, 2, 7, 1, 4, 0, 8 ]
       expect{ fb_tree.preorder }.to output("6\n3\n5\n9\n2\n7\n1\n4\n0\n8\n").to_stdout
+    end
+  end
+  describe "#inorder" do
+    it 'outputs values' do
+      # [ 9, 5, 2, 7, 3, 1, 6, 4, 8, 0 ]
+      expect{ fb_tree.inorder }.to output("9\n\n5\n2\n7\n\n\n\n3\n1\n\n\n6\n4\n8\n\n0\n\n\n").to_stdout
+    end
+  end
+  describe "#postorder" do
+    it 'outputs values' do
+      # [ 9, 7, 2, 5, 1, 3, 8, 0, 4, 6 ]
+      expect{ fb_tree.postorder }.to output("9\n\n7\n\n2\n\n5\n\n1\n\n3\n\n8\n\n0\n\n4\n\n6\n").to_stdout
     end
   end
 end
