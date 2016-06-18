cfea908e229b76401a4b377ecc9694e00f5f4917
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-18 18:10:02 -0400

    just 2

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -29,3 +29,18 @@ end
           #v              v
           #1              0
 
+class Fibonacci
+  def self.of(n)
+    fail "cant use negatives" if n < 0
+    return n if n >= 0 && n < 2
+    first, second = 0, 1
+    next_val = nil
+
+    (2..n).each do |i|
+      next_val = first + second
+      first, second = second, next_val
+    end
+
+    next_val
+  end
+end
