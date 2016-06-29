fcfb6fd71e49a5b38f0c05cfe869056ac8611bfc
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-29 12:59:00 -0400

    scanr

--- spec/scan_spec.rb
+++ spec/scan_spec.rb
@@ -9,11 +9,26 @@ class Array
     end
     results
   end
+  def scanr accumulator, &block
+    results = self + [accumulator]
+    n = results.size
+    (n - 1).downto(0) do |i|
+      results[i] = block.call(accumulator, results[i])
+      accumulator = results[i]
+    end
+    results
+  end
 end
 
 describe "#scanl" do
   it 'is similar to foldl, but returns a list of successive reduced values from the left' do
     # example from http://learnyouahaskell.com/higher-order-functions
     expect([3, 5, 2, 1].scanl(0, &:+)).to eq([0,3,8,10,11])
+  end
+end
+
+describe "#scanr" do
+  it 'report all the intermediate accumulator states in the form of a list from right' do
+    expect([3, 5, 2, 1].scanr(0, &:+)).to eq([11,8,3,1,0])
   end
 end
