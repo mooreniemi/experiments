d65975820d6d221c9bb52f26c5f0eed03de061c8
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-07-04 16:58:46 -0400

    all cases

--- lib/find_max.rb
+++ lib/find_max.rb
@@ -1,23 +1,26 @@
+#array increases up to a certain point then starts decreasing again
+#1, 2, 3, 7, 6, 5
+#find peak within log n time
 class Array
-  def find_max
-    pivot = (self.length / 2).floor
-    list = self
-    max = nil
-    while max.nil?
+	def find_max
+		pivot = (self.length / 2)
+		list = self
+		max = nil
+		while max.nil?
 			# fetching in case we dont have any more elements higher or lower
-      if list[pivot] > list.fetch(pivot - 1,0) && list[pivot] > list.fetch(pivot + 1,0)
-        max = list[pivot]
-      else
-        # if increasing still
-        if list[pivot] > list[pivot - 1]
-          list = list[pivot..-1]
-          pivot = (list.length / 2).floor
-        else
-          list = list[0..pivot]
-          pivot = (list.length / 2).floor
-        end
-      end
-    end
-    max
-  end
+			if list[pivot] >= list.fetch(pivot - 1,0) && list[pivot] >= list.fetch(pivot + 1,0)
+				max = list[pivot]
+			else
+				# if increasing still
+				if list[pivot] > list.fetch(pivot - 1, 0)
+					list = list[pivot..-1]
+					pivot = (list.length / 2)
+				else
+					list = list[0..pivot - 1]
+					pivot = (list.length / 2)
+				end
+			end
+		end
+		max
+	end
 end
--- spec/find_max_spec.rb
+++ spec/find_max_spec.rb
@@ -3,20 +3,28 @@ require 'rantly/rspec_extensions'
 require 'find_max'
 
 describe Array do
-  describe '#find_max' do
-    it 'returns max element in camel list' do
-      expect([1,3,2].find_max).to eq(3)
-      expect([1,2,3,7,6,5].find_max).to eq(7)
-    end
-    it 'satisfies property test' do
-      property_of {
-        len_one = range(1,49)
-        len_two = range(1,49)
-        (array(len_one) { range(1,998) }.sort + [999] +
-          array(len_two) { range(1,998) }.sort { |x,y| y <=> x }).uniq
-      }.check(1000) {|array|
-        expect(array.find_max).to eq(999)
-      }
-    end
-  end
+	describe '#find_max' do
+		it 'returns max element in camel list' do
+			expect([1,3,2].find_max).to eq(3)
+			expect([1,2,3,7,6,5].find_max).to eq(7)
+		end
+		it 'returns single element' do
+			expect([999].find_max).to eq(999)
+		end
+		it 'descending only' do
+			expect([999, 897, 678, 616, 479, 333, 225, 150].find_max).to eq(999)
+		end
+		it 'satisfies property test' do
+			property_of {
+				len_one = range(0,49)
+				len_two = range(0,49)
+				(array(len_one) { range(1,998) }.sort + [999] +
+				 array(len_two) { range(1,998) }.sort { |x,y| y <=> x }).uniq
+			}.check(1000) {|array|
+				# used to output case if im in infinite loop
+				puts array.inspect
+				expect(array.find_max).to eq(999)
+			}
+		end
+	end
 end
