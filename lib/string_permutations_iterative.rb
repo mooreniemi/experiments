b0bf6e5d007474c871245b1bb41d0475f6df43bf
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-07-05 18:17:33 -0400

    to_s for PermuteOp

--- lib/string_permutations_iterative.rb
+++ lib/string_permutations_iterative.rb
@@ -2,18 +2,24 @@
 
 class String
 
-  Struct.new("PermuteOp", :prefix, :rest)
+  PermuteOp = Struct.new(:prefix, :rest) do
+    def to_s
+      "prefix: #{prefix}, rest: #{rest}"
+    end
+  end
 
   def permutations
     strings = []
-    stack = [Struct::PermuteOp.new("", self)]
+    stack = [PermuteOp.new("", self)]
 
     while !stack.empty?
       s = stack.pop
       s.rest.length.times do |i|
         copy = s.rest.dup
         prefix = copy.slice!(i)
-        stack.push(Struct::PermuteOp.new(s.prefix + prefix, copy))
+        new_op = PermuteOp.new(s.prefix + prefix, copy)
+        #puts new_op
+        stack.push(new_op)
       end
       strings << s.prefix if s.rest == ""
     end
