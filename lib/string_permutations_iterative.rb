060a6590157d29bf238293692bb9b9fd2d1f39e3
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-07-05 21:47:44 -0400

    with property test for iterative permutations

--- lib/string_permutations_iterative.rb
+++ lib/string_permutations_iterative.rb
@@ -1,7 +1,7 @@
 # http://www.geeksforgeeks.org/permutations-string-using-iteration/
 
 class String
-
+  # this simulates what goes to the call stack
   PermuteOp = Struct.new(:prefix, :rest) do
     def to_s
       "prefix: #{prefix}, rest: #{rest}"
@@ -17,13 +17,16 @@ class String
       s.rest.length.times do |i|
         copy = s.rest.dup
         prefix = copy.slice!(i)
+
         new_op = PermuteOp.new(s.prefix + prefix, copy)
-        #puts new_op
+        puts new_op
+
         stack.push(new_op)
       end
+
       strings << s.prefix if s.rest == ""
     end
 
-    strings.uniq
+    strings
   end
 end
--- spec/string_permutations_iterative_spec.rb
+++ spec/string_permutations_iterative_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'rantly/rspec_extensions'
 require 'string_permutations_iterative'
 
 # for checking test cases easily
@@ -6,12 +7,47 @@ require 'string_permutations_iterative'
 
 describe String do
   describe "#permutations" do
-    it 'returns all permutations of a string' do
+    it 'does the minimum swap for 2 elements' do
+      expect('ab'.permutations).to match_array(%w{ ab ba })
+    end
+    it 'returns all permutations of a length 3 string' do
       expect('ABC'.permutations).to match_array(%w{ ABC ACB BAC BCA CBA CAB })
     end
-    it 'works for any size of string' do
+    it 'works for strings of length 4 and 5' do
       expect('abcd'.permutations).to match_array(%w{ abcd abdc acbd acdb adbc adcb bacd badc bcad bcda bdac bdca cabd cadb cbad cbda cdab cdba dabc dacb dbac dbca dcab dcba })
-      expect('doggo'.permutations).to match_array(%w{ doogg dogog doggo dgoog dgogo dggoo odogg odgog odggo oodgg oogdg ooggd ogdog ogdgo ogodg ogogd oggdo oggod gdoog gdogo gdgoo godog godgo goodg googd gogdo gogod ggdoo ggodo ggood })
+      expect('doggo'.permutations).to match_array([
+        "dggoo", "dggoo", "dggoo", "dggoo", "dgogo",
+        "dgogo", "dgogo", "dgogo", "dgoog", "dgoog",
+        "dgoog", "dgoog", "doggo", "doggo", "doggo",
+        "doggo", "dogog", "dogog", "dogog", "dogog",
+        "doogg", "doogg", "doogg", "doogg", "gdgoo",
+        "gdgoo", "gdgoo", "gdgoo", "gdogo", "gdogo",
+        "gdogo", "gdogo", "gdoog", "gdoog", "gdoog",
+        "gdoog", "ggdoo", "ggdoo", "ggdoo", "ggdoo",
+        "ggodo", "ggodo", "ggodo", "ggodo", "ggood",
+        "ggood", "ggood", "ggood", "godgo", "godgo",
+        "godgo", "godgo", "godog", "godog", "godog",
+        "godog", "gogdo", "gogdo", "gogdo", "gogdo",
+        "gogod", "gogod", "gogod", "gogod", "goodg",
+        "goodg", "goodg", "goodg", "googd", "googd",
+        "googd", "googd", "odggo", "odggo", "odggo",
+        "odggo", "odgog", "odgog", "odgog", "odgog",
+        "odogg", "odogg", "odogg", "odogg", "ogdgo",
+        "ogdgo", "ogdgo", "ogdgo", "ogdog", "ogdog",
+        "ogdog", "ogdog", "oggdo", "oggdo", "oggdo",
+        "oggdo", "oggod", "oggod", "oggod", "oggod",
+        "ogodg", "ogodg", "ogodg", "ogodg", "ogogd",
+        "ogogd", "ogogd", "ogogd", "oodgg", "oodgg",
+        "oodgg", "oodgg", "oogdg", "oogdg", "oogdg",
+        "oogdg", "ooggd", "ooggd", "ooggd", "ooggd"
+      ])
+    end
+    it 'returns the same values as the core lib function' do
+      property_of {
+        string
+      }.check(100) {|string|
+        expect(string.permutations).to match_array(string.chars.permutation.map(&:join))
+      }
     end
   end
 end
