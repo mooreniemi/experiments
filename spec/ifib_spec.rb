386118de938774732e160c73bd1a3617d47f5c52
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-07-07 13:31:17 -0400

    as refinement

--- spec/ifib_spec.rb
+++ spec/ifib_spec.rb
@@ -1,35 +1,50 @@
 require 'spec_helper'
 
-# https://www.mathsisfun.com/numbers/fibonacci-sequence.html
-class Fixnum
-  def fibonacci_r
-    return self if self <= 1
-    (self - 1).fibonacci_r + (self - 2).fibonacci_r
-  end
-
-  def fibonacci_s
-    sum = 0
-    stack = [self]
-    while !stack.empty?
-      # not really a case, haha, for a case statement
-      # but wanted to play with using lambdas in one
-      # http://blog.honeybadger.io/rubys-case-statement-advanced-techniques/
-      case e = stack.pop
-      when -> (n) { n <= 0 }
-      when -> (n) { n == 1 }
-        sum += e
-        puts "sum: #{sum}"
-      else
-        stack.push(e - 1, e - 2)
-        puts "stack: #{stack}"
+module Fibonacci
+  refine Fixnum do
+    class ZeroOrBelow
+      def self.===(i)
+        i <= 0
       end
     end
-    sum
+
+    class One
+      def self.===(i)
+        i == 1
+      end
+    end
+
+    def fibonacci_r
+      return self if self <= 1
+      (self - 1).fibonacci_r + (self - 2).fibonacci_r
+    end
+
+    def fibonacci_s
+      sum = 0
+      stack = [self]
+      while !stack.empty?
+        case e = stack.pop
+        when ZeroOrBelow #-> (n) { n <= 0 }
+          # http://blog.honeybadger.io/rubys-case-statement-advanced-techniques/
+          # commented after is the lambda version
+        when One #-> (n) { n == 1 }
+          sum += e
+          puts "sum: #{sum}"
+        else
+          stack.push(e - 1, e - 2)
+          puts "stack: #{stack}"
+        end
+      end
+      sum
+    end
   end
 end
 
 describe Fixnum do
+  using Fibonacci
+  # https://www.mathsisfun.com/numbers/fibonacci-sequence.html
   let(:series) { [0,1,1,2,3,5,8,13,21] }
+
   describe "#fibonacci_r" do
     it 'recursively give series number' do
       series.each_with_index do |fib_num, i|
@@ -37,6 +52,7 @@ describe Fixnum do
       end
     end
   end
+
   describe "#fibonacci_s" do
     it 'uses stack to give series number' do
       series.each_with_index do |fib_num, i|
