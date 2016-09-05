ff3feeea6bde1eda7d091cb11adc9f882f955bfc
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-09-04 21:02:56 -0400

    playing more with bfs

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,131 +1,149 @@
 module Heapify
-	# TODO
-	# https://en.wikipedia.org/wiki/Binary_heap#Insert
-	def insert
-		#Add the element to the bottom level of the heap.
-		#Compare the added element with its parent; if they are in the correct order, stop.
-		#If not, swap the element with its parent and return to the previous step.
-	end
+  # TODO
+  # https://en.wikipedia.org/wiki/Binary_heap#Insert
+  def insert
+    #Add the element to the bottom level of the heap.
+    #Compare the added element with its parent; if they are in the correct order, stop.
+    #If not, swap the element with its parent and return to the previous step.
+  end
 end
 
 module Breadth
-	attr_accessor :distance
-	attr_accessor :parent
-	attr_accessor :as_rows
+  attr_accessor :distance
+  attr_accessor :parent
+  attr_accessor :rows, :height
 
-	def as_rows
-		@as_rows ||= []
-	end
+  def height
+    @height ||= 0
+  end
 
-	def distance
-		@distance ||= Float::INFINITY
-	end
+  def rows
+    @rows ||= []
+  end
 
-	def parent
-		@parent ||= nil
-	end
+  def as_rows
+    bfs_for(self)
+    rows_copy = self.rows.dup
+    rowed = []
+    self.height.times do |n|
+      if n == 1
+        rowed << rows_copy.slice!(0..1)
+      else
+        rowed << rows_copy.slice!(0..2**n + 1)
+      end
+    end
+    rowed
+  end
 
-	def bfs_for(element)
-		self.distance = 0
-		root = self
-		queue = [root]
-		visited = []
+  def distance
+    @distance ||= Float::INFINITY
+  end
 
-		until queue.empty?
-			current = queue.shift
-			adjacency_list = current.children
+  def parent
+    @parent ||= nil
+  end
 
-			adjacency_list.each do |node|
-				root.as_rows << (node.nil? ? "x" : node.value)
-				next if node.nil?
-				visited << node.value
+  def bfs_for(element)
+    root = self
+    queue = [root]
+    visited = []
 
-				if node.distance == Float::INFINITY
-					node.distance = current.distance + 1
-					node.parent = current
-					queue.push(node)
-				end
-			end
-		end
+    until queue.empty?
+      current = queue.shift
+      adjacency_list = current.children
 
-		visited.include?(element)
-	end
+      self.height += 1
+      adjacency_list.each do |node|
+        root.rows << (node.nil? ? "x" : node.value)
+        next if node.nil?
+        visited << node.value
+
+        if node.distance == Float::INFINITY
+          node.distance = current.distance + 1
+          node.parent = current
+          queue.push(node)
+        end
+      end
+    end
+
+    visited.include?(element)
+  end
 end
 
 module Coordinates
-	attr_accessor :x
-	attr_accessor :y
-	attr_accessor :xy
+  attr_accessor :x
+  attr_accessor :y
+  attr_accessor :xy
 
-	def xy
-		@xy ||= [0,0]
-	end
+  def xy
+    @xy ||= [0,0]
+  end
 
-	def x
-		xy.first
-	end
+  def x
+    xy.first
+  end
 
-	def y
-		xy.last
-	end
+  def y
+    xy.last
+  end
 
 
-	def with_coordinates
-		inorder {|n, i = 0| i += 1; n.x = i; n.y = i}
-	end
+  def with_coordinates
+    inorder {|n, i = 0| i += 1; n.x = i; n.y = i}
+  end
 end
 
 class Node
-	include Breadth
-	include Coordinates
+  include Breadth
+  include Coordinates
 
-	attr_accessor :left
-	attr_accessor :right
-	attr_accessor :value
+  attr_accessor :left
+  attr_accessor :right
+  attr_accessor :value
 
-	def initialize(value: nil, left: nil, right: nil)
-		@value = value
-		@left = left
-		@right = right
-	end
+  def initialize(value: nil, left: nil, right: nil)
+    @value = value
+    @left = left
+    @right = right
+  end
 
-	def preorder(current = self, &block)
-		yield current if block
-		preorder(current.left, &block) unless current.left.nil?
-		preorder(current.right, &block) unless current.right.nil?
-	end
+  def preorder(current = self, &block)
+    yield current if block
+    preorder(current.left, &block) unless current.left.nil?
+    preorder(current.right, &block) unless current.right.nil?
+  end
 
-	def inorder(current = self, &block)
-		inorder(current.left, &block) unless current.left.nil?
-		yield current if block
-		inorder(current.right, &block) unless current.right.nil?
-	end
+  def inorder(current = self, &block)
+    inorder(current.left, &block) unless current.left.nil?
+    yield current if block
+    inorder(current.right, &block) unless current.right.nil?
+  end
 
-	def postorder(current = self, &block)
-		postorder(current.left, &block) unless current.left.nil?
-		postorder(current.right, &block) unless current.right.nil?
-		yield current if block
-	end
+  def postorder(current = self, &block)
+    postorder(current.left, &block) unless current.left.nil?
+    postorder(current.right, &block) unless current.right.nil?
+    yield current if block
+  end
 
-	# https://leetcode.com/problems/maximum-depth-of-binary-tree/
-	def height(current = self)
-		return 0 if current.nil?
-		lh = height(current.left)
-		rh = height(current.right)
-		[lh + 1, rh + 1].max
-	end
+  # https://leetcode.com/problems/maximum-depth-of-binary-tree/
+  def height(current = self)
+    return 0 if current.nil?
+    lh = height(current.left)
+    rh = height(current.right)
+    [lh + 1, rh + 1].max
+  end
 
-	def invert(current = self)
-		return current if children.empty?
-		invert(current.left) unless current.left.nil?
-		invert(current.right) unless current.right.nil?
-		l = current.left
-		current.left = current.right
-		current.right = l
-		current
-	end
+  def invert(current = self)
+    return current if children.empty?
+    invert(current.left) unless current.left.nil?
+    invert(current.right) unless current.right.nil?
+    l = current.left
+    current.left = current.right
+    current.right = l
+    current
+  end
 
-	def children
-		[left, right]
-	end
+  def children
+    [left, right]
+  end
 end
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -5,116 +5,110 @@ require 'tree_data'
 include TreeData
 
 describe "Tree" do
-	describe "#children" do
-		it 'gives left and right together' do
-			expect(fb_tree.children.size).to eq(2)
-		end
-	end
-	describe "#preorder" do
-		it 'outputs values' do
-			# [ 6, 3, 5, 9, 2, 7, 1, 4, 0, 8 ]
-			expect{ fb_tree.preorder {|n| puts "#{n.value}" } }.
-				to output("6\n3\n5\n9\n2\n7\n1\n4\n0\n8\n").to_stdout
-		end
-	end
-	describe "#inorder" do
-		it 'outputs values' do
-			# [ 9, 5, 2, 7, 3, 1, 6, 4, 8, 0 ]
-			expect { fb_tree.inorder {|n| puts "#{n.value}" } }.
-				to output("9\n5\n2\n7\n3\n1\n6\n4\n8\n0\n").to_stdout
-		end
-	end
-	describe "#postorder" do
-		it 'outputs values' do
-			# [ 9, 7, 2, 5, 1, 3, 8, 0, 4, 6 ]
-			expect{ fb_tree.postorder {|n| puts "#{n.value}" } }.
-				to output("9\n7\n2\n5\n1\n3\n8\n0\n4\n6\n").to_stdout
-		end
-	end
+  describe "#children" do
+    it 'gives left and right together' do
+      expect(fb_tree.children.size).to eq(2)
+    end
+  end
+  describe "#preorder" do
+    it 'outputs values' do
+      # [ 6, 3, 5, 9, 2, 7, 1, 4, 0, 8 ]
+      expect{ fb_tree.preorder {|n| puts "#{n.value}" } }.
+        to output("6\n3\n5\n9\n2\n7\n1\n4\n0\n8\n").to_stdout
+    end
+  end
+  describe "#inorder" do
+    it 'outputs values' do
+      # [ 9, 5, 2, 7, 3, 1, 6, 4, 8, 0 ]
+      expect { fb_tree.inorder {|n| puts "#{n.value}" } }.
+        to output("9\n5\n2\n7\n3\n1\n6\n4\n8\n0\n").to_stdout
+    end
+  end
+  describe "#postorder" do
+    it 'outputs values' do
+      # [ 9, 7, 2, 5, 1, 3, 8, 0, 4, 6 ]
+      expect{ fb_tree.postorder {|n| puts "#{n.value}" } }.
+        to output("9\n7\n2\n5\n1\n3\n8\n0\n4\n6\n").to_stdout
+    end
+  end
 
-	describe "#height" do
-		it 'or depth' do
-			expect(fb_tree.height).to eq(5)
-			expect(Node.new(value: 1).height).to eq(1)
-			expect(Node.new(value: 1, left: Node.new(value: 2)).height).to eq(2)
-		end
-	end
+  describe "#height" do
+    it 'or depth' do
+      expect(fb_tree.height).to eq(5)
+      expect(Node.new(value: 1).height).to eq(1)
+      expect(Node.new(value: 1, left: Node.new(value: 2)).height).to eq(2)
+    end
+  end
 
 
-	RSpec::Matchers.define :be_value_equivalent_of do |expected|
-		match do |actual|
-			actual_traversed = []
-			actual.inorder {|node| actual_traversed << node.value }
-			expected_traversed = []
-			expected.inorder {|node| expected_traversed << node.value }
+  RSpec::Matchers.define :be_value_equivalent_of do |expected|
+    match do |actual|
+      actual_traversed = []
+      actual.inorder {|node| actual_traversed << node.value }
+      expected_traversed = []
+      expected.inorder {|node| expected_traversed << node.value }
 
-			actual_traversed == expected_traversed
-		end
-	end
+      actual_traversed == expected_traversed
+    end
+  end
 
-	describe "#invert" do
-		it 'reverses right and left' do
-			expect(google_tree.invert).to be_value_equivalent_of(inverted_google_tree)
-		end
-	end
+  describe "#invert" do
+    it 'reverses right and left' do
+      expect(google_tree.invert).to be_value_equivalent_of(inverted_google_tree)
+    end
+  end
 
-	describe "columning" do
-		# https://www.careercup.com/question?id=5749533368647680
-		# Given the root of a binary tree containing integers, print the columns of the tree in order with the nodes in each column printed top-to-bottom.
+  describe "columning" do
+    # https://www.careercup.com/question?id=5749533368647680
+    # Given the root of a binary tree containing integers, print the columns of the tree in order with the nodes in each column printed top-to-bottom.
 
-		context "using coordinates strategy" do
-			describe "#with_coordinates" do
-				let(:base_tree) { Node.new(value: 1) }
-				it 'assigns base_tree [0,0]' do
-					puts base_tree.with_coordinates
-				end
-				it 'assigns left child [1,1]' do
-					base_tree.left = Node.new(value: 2)
-				end
-			end
-		end
+    context "using coordinates strategy" do
+      describe "#with_coordinates" do
+        let(:base_tree) { Node.new(value: 1) }
+        it 'assigns base_tree [0,0]' do
+          puts base_tree.with_coordinates
+        end
+        it 'assigns left child [1,1]' do
+          base_tree.left = Node.new(value: 2)
+        end
+      end
+    end
 
-		context "using padding strategy" do
-			# TODO non-coordinates strategy
-			# bfs to get rows and height and keep placeholders
-			# get max width based on height from bfs
-			# pad all rows
+    context "using padding strategy" do
+      # TODO non-coordinates strategy
+      # bfs to get rows and height and keep placeholders
+      # get max width based on height from bfs
+      # pad all rows
 
-			describe "#bfs_for(element)" do
-				it 'does BFS' do
-					expect(fb_tree.bfs_for(7)).to be true
-					expect(fb_tree.bfs_for(17)).to be false
-				end
+      describe "#bfs_for(element)" do
+        it 'does BFS' do
+          expect(fb_tree.bfs_for(7)).to be true
+          expect(fb_tree.bfs_for(17)).to be false
+        end
 
-				it 'populates rows' do
-					sticky_tree = fb_tree
-					sticky_tree.bfs_for(99)
-					rows = sticky_tree.as_rows
+        it 'populates rows' do
+          sticky_tree = fb_tree.dup
+          sticky_tree.bfs_for(99)
+          rows = sticky_tree.as_rows
+          expect(rows.uniq.size).to eq(5)
+        end
 
-					# TODO need to partition list
-					height = 5
-					puts rows.slice!(0..2**0)
-					puts "\n"
-					(height - 1).times do |n|
-						n += 1
-						puts rows.slice!(0..2**n + 1)
-						puts "\n"
-					end
+        it 'populates height' do
+          fb_tree.bfs_for(7)
+          expect(fb_tree.height).to eq(5)
+        end
+      end
 
-					expect(rows.uniq.size).to eq(5)
-				end
-			end
+      it 'represents trees as levels' do
+        top = [nil, nil, 6, nil, nil]
+        middle = [nil, 3, nil, nil, 4, nil]
+        bottom = [5, nil, 1, nil, nil, 0]
 
-			it 'represents trees as levels' do
-				top = [nil, nil, 6, nil, nil]
-				middle = [nil, 3, nil, nil, 4, nil]
-				bottom = [5, nil, 1, nil, nil, 0]
-
-				first_column = [top[0]] + [middle[0]] + [bottom[0]]
-				second_column = [top[2]] + [middle[2]] + [bottom[2]]
-				expect(first_column).to eq([nil, nil, 5])
-				expect(second_column).to eq([6, nil, 1])
-			end
-		end
-	end
+        first_column = [top[0]] + [middle[0]] + [bottom[0]]
+        second_column = [top[2]] + [middle[2]] + [bottom[2]]
+        expect(first_column).to eq([nil, nil, 5])
+        expect(second_column).to eq([6, nil, 1])
+      end
+    end
+  end
 end
