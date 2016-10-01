041e94a0f082fcc4143b13ebc95d9a0f5a2a45a6
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-10-01 15:10:42 -0400

    map theorem

--- spec/compose_spec.rb
+++ spec/compose_spec.rb
@@ -72,6 +72,9 @@ describe '#compose' do
     composition = triple * double * add_one
     expect(composition.(2)).to eq(18)
   end
+  it 'obeys map f (map g xs) = map (f . g) xs' do
+    expect([1,2].map(&double).map(&triple)).to eq([1,2].map(&(double * triple)))
+  end
 end
 
 describe '#blackbird' do
