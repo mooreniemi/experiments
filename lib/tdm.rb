f3bd29b391e1bfe1c9b4898f79c11ce0632e77db
Alex Moore-Niemi <alex.moore-niemi@raizlabs.com>
2016-12-09 15:59:09 -0500

    tdm frozen

--- lib/tdm.rb
+++ lib/tdm.rb
@@ -1,6 +1,9 @@
 # http://mooreniemi.github.io/js/programming/2016/11/28/table-driven-methods.html
 require 'graph/function'
 
+Graph::Function.configure do |config|
+  config.trials = 1000
+end
 Graph::Function.as_gif(File.expand_path('../tdm.gif', __FILE__))
 
 array_generator = proc {|size| Array.new(size) { [] } }
@@ -14,9 +17,9 @@ def tdm(a)
   live = proc {|c| c.push(1) }
   die = proc {|c| c.clear }
 
-  next_live_state = [die, die, live, live, die]
-  next_dead_state = [die, die, die, live, die]
-  next_state = [next_dead_state, next_live_state]
+  next_live_state = [die, die, live, live, die].freeze
+  next_dead_state = [die, die, die, live, die].freeze
+  next_state = [next_dead_state, next_live_state].freeze
 
   a.each {|e| next_state[is_live][num_live_neighbors].(e) }
 end
