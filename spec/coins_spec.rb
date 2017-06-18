a631e46ab253dea7949fcf217e2f04b4d6e0158b
alex <bigbadmath@gmail.com>
2017-06-18 12:45:17 -0400

    coins with optimal space

---
+++ spec/coins_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+def coins(denoms, target)
+  table = Array.new(target) { 0 }
+  # base case, 1 way to make 0
+  table.unshift(1)
+
+  0.upto(denoms.size - 1) do |i|
+    denoms[i].upto(target) do |j|
+      p table
+      table[j] += table[j-denoms[i]]
+    end
+  end
+
+  table[target]
+end
+
+describe 'find all ways to make change' do
+  it 'there are 5 ways to make 10' do
+    expect(coins([2, 5, 3, 6], 10)).to eq(5)
+  end
+  it 'there are 4 ways to make make 4' do
+    expect(coins([1, 2, 3], 4)).to eq(4)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'pry'
+require 'awesome_print'
 
 # give us some debug info when running rspec
 ENV['TEST_ENV'] = 'true'
